11cc8d530c0156e5a2ad501a7cc2f39ed80f8dff
taznimt <taznimanwar06@gmail.com>
2025-11-25 10:04:00 +0700

    Create Komik model with fields for id, title, description, author, and image data

---
+++ models/komik.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define("Komik", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.STRING,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        author: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        imageType: DataTypes.STRING,
+        imageName: DataTypes.STRING,
+        imageData: DataTypes.BLOB('long'),
+    },
+    {
+        tableName : "komik",
+
+    });
+    return Komik;
+}
