f0eb54c4f140a12ad04575805363144bcc27f167
taznimt <taznimanwar06@gmail.com>
2025-11-25 11:05:46 +0700

    Set API base path to /api/v1

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use('/api/v1', apiRoute)
 
 async function startServer(){
     await conectDatabase();
